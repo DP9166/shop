a3c1df32d0d5bd6d61c20905720fbc0cf439e823
段沛 <149022511@qq.com>
2019-04-30 18:17:45 +0800

    收货地址

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
-Route::get('/', 'PagesController@root')->name('root')->middleware('verified');
+Route::get('/', 'PagesController@root')->name('root');
 
 Auth::routes(['verify' => true]);
+
+Route::group(['middleware' => ['auth']], function() {
+    Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
+});
