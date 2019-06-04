2553cc812e557c9962e933de18505839d552ddd6
duanpei <duanpei@duanpeideMacBook-Pro.local>
2019-06-05 00:58:34 +0800

    分面搜索

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -35,7 +35,6 @@ class ProductsController extends Controller
             ]
         ];
 
-
         if ($order = $request->input('order', '')) {
             if (preg_match('/^(.+)_(asc|desc)$/', $order, $m)) {
                 if (in_array($m[1], ['price', 'sold_count', 'rating'])) {
@@ -79,6 +78,52 @@ class ProductsController extends Controller
             }
         }
 
+
+        if ($search || isset($category)) {
+            $params['body']['aggs'] = [
+                'properties'    =>  [
+                    'nested'    =>  [
+                        'path'  =>  'properties'
+                    ],
+                    'aggs'  =>  [
+                        'properties'    =>  [
+                            'terms' =>  [
+                                'field' =>  'properties.name'
+                            ],
+                            'aggs' =>   [
+                                'value' =>  [
+                                    'terms' =>  [
+                                        'field' =>  'properties.value'
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            ];
+        }
+
+        $propertyFilters = [];
+        if ($filterString = $request->input('filters')) {
+            $filterArray = explode('|', $filterString);
+
+            foreach ($filterArray as $filter) {
+                list($name, $value) = explode(':', $filter);
+
+                $propertyFilters[$name] = $value;
+                $params['body']['query']['bool']['filter'][] = [
+                    'nested'    =>  [
+                        'path'  =>  'properties',
+                        'query' =>  [
+                            ['term' =>  ['properties.name'  =>  $name]],
+                            ['term' =>  ['properties.value'  =>  $value]],
+                        ],
+                    ]
+                ];
+
+            }
+        }
+
         $result = app('es')->search($params);
 
         $productIds = collect($result['hits']['hits'])->pluck('_id')->all();
@@ -89,6 +134,20 @@ class ProductsController extends Controller
             'path'  =>  route('products.index', false), // 手动构建分页的 url
         ]);
 
+        if (isset($result['aggregations'])) {
+            $properties = collect($result['aggregations']['properties']['properties']['buckets'])
+                ->map(function ($bucket) {
+                    return [
+                        'key'        =>  $bucket['key'],
+                        'values'     =>  collect($bucket['value']['buckets'])->pluck('key')->all()
+                    ];
+                })->filter(function ($property) use ($propertyFilters) {
+                    // 过滤只剩下一个值 或者 已经在筛选条件里的属性
+                    return count($property['values']) > 1 &&
+                        !isset($propertyFilters[$property['key']]);
+                });
+        }
+
 
         return view('products.index', [
             'products'=>$pager,
@@ -97,8 +156,9 @@ class ProductsController extends Controller
                 'order' => $order,
             ],
             // 等价于 isset($category) ? $category : null
-            'category' => $category ?? null,
-            'categoryTree'  => $categoryService->getCategoryTree()
+            'category'      => $category ?? null,
+            'categoryTree'  => $categoryService->getCategoryTree(),
+            'properties'    =>  $properties
         ]);
     }
 
